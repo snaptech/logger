a13e01761c5b039226f3205b4fefc6197008362e
Tim Erwin <tim.erwin@gmail.com>
2016-12-07 15:45:55 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const winston = require('winston');
+
+const logger = new winston.Logger({
+  transports: [
+    new winston.transports.File({
+      level: 'info',
+      filename: './logs/system.log',
+      handleExceptions: true,
+      humanReadableUnhandledException: true,
+      json: true,
+      maxsize: 5242880, //5MB
+      maxFiles: 5,
+      colorize: false,
+      timestamp: true
+    }),
+    new winston.transports.Console({
+      level: 'info',
+      handleExceptions: true,
+      humanReadableUnhandledException: false,
+      json: false,
+      colorize: true,
+      timestamp: true
+    })
+  ],
+  exitOnError: false
+});
+
+const safeLog = (method, ...args) => {
+  try {
+    return logger[method](...args);
+  } catch (err) {
+    console.error(err);
+  }
+
+  return null;
+};
+
+//proxied to abstract logger for easy library replacement
+module.exports = {
+  trace: (...args) => safeLog('debug', ...args),
+  debug: (...args) => safeLog('debug', ...args),
+  info: (...args) => safeLog('info', ...args),
+  warn: (...args) => safeLog('warn', ...args),
+  error: (...args) => safeLog('error', ...args[0].message, ...args)
+};
